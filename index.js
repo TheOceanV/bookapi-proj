1bce7424e14f0f9e5e60e5e29aef6bc9f00d7ae9
TheOceanV <dataguru.2016@gmail.com>
2021-09-26 18:35:18 +0530

    add Pub PUT api

---
+++ index.js
@@ -0,0 +1,298 @@
+// Frame work
+const express = require("express");
+
+// Database
+const database = require("./database/index");
+
+// Initializing express
+const shapeAI = express();
+
+// Configurations
+shapeAI.use(express.json());
+
+/*
+Route           /
+Description     get all books
+Access          PUBLIC
+Parameters      NONE
+Method          GET
+*/
+shapeAI.get("/", (req, res) => {
+  return res.json({ books: database.books });
+});
+
+/*
+Route           /is
+Description     get specific book based on ISBN
+Access          PUBLIC
+Parameters      isbn
+Method          GET
+*/
+shapeAI.get("/is/:isbn", (req, res) => {
+  const getSpecificBook = database.books.filter(
+    (book) => book.ISBN === req.params.isbn
+  );
+
+  if (getSpecificBook.length === 0) {
+    return res.json({
+      error: `No book found for the ISBN of ${req.params.isbn}`,
+    });
+  }
+
+  return res.json({ book: getSpecificBook });
+});
+
+/*
+Route           /c
+Description     get specific books based on a category
+Access          PUBLIC
+Parameters      category
+Method          GET
+*/
+shapeAI.get("/c/:category", (req, res) => {
+  const getSpecificBooks = database.books.filter((book) =>
+    book.category.includes(req.params.category)
+  );
+
+  if (getSpecificBooks.length === 0) {
+    return res.json({
+      error: `No book found for the category of ${req.params.category}`,
+    });
+  }
+
+  return res.json({ books: getSpecificBooks });
+});
+
+/*
+Route           /author
+Description     get all authors
+Access          PUBLIC
+Parameters      NONE
+Method          GET
+*/
+shapeAI.get("/author", (req, res) => {
+  return res.json({ authors: database.authors });
+});
+
+/*
+Route           /author
+Description     get a list of authors based on a book's ISBN
+Access          PUBLIC
+Parameters      isbn
+Method          GET
+*/
+shapeAI.get("/author/:isbn", (req, res) => {
+  const getSpecificAuthors = database.authors.filter((author) =>
+    author.books.includes(req.params.isbn)
+  );
+
+  if (getSpecificAuthors.length === 0) {
+    return res.json({
+      error: `No author found for the book ${req.params.isbn}`,
+    });
+  }
+
+  return res.json({ authors: getSpecificAuthors });
+});
+
+/*
+Route           /publications
+Description     get all publications
+Access          PUBLIC
+Parameters      NONE
+Method          GET
+*/
+shapeAI.get("/publications", (req, res) => {
+  return res.json({ publications: database.publications });
+});
+
+/*
+Route           /book/new
+Description     add new books
+Access          PUBLIC
+Parameters      NONE
+Method          POST
+*/
+shapeAI.post("/book/new", (req, res) => {
+  const { newBook } = req.body;
+  database.books.push(newBook);
+  return res.json({ books: database.books, message: "book was added!" });
+});
+
+/*
+Route           /author/new
+Description     add new author
+Access          PUBLIC
+Parameters      NONE
+Method          POST
+*/
+shapeAI.post("/author/new", (req, res) => {
+  const { newAuthor } = req.body;
+  database.authors.push(newAuthor);
+  return res.json({ authors: database.authors, message: "author was added!" });
+});
+
+/*
+Route           /book/update
+Description     update title of a book
+Access          PUBLIC
+Parameters      isbn
+Method          PUT
+*/
+shapeAI.put("/book/update/:isbn", (req, res) => {
+  database.books.forEach((book) => {
+    if (book.ISBN === req.params.isbn) {
+      book.title = req.body.bookTitle;
+      return;
+    }
+  });
+
+  return res.json({ books: database.books });
+});
+
+/*
+Route           /book/author/update
+Description     update/add new author
+Access          PUBLIC
+Parameters      isbn
+Method          PUT
+*/
+shapeAI.put("/book/author/update/:isbn", (req, res) => {
+  // update the book database
+  database.books.forEach((book) => {
+    if (book.ISBN === req.params.isbn)
+      return book.authors.push(req.body.newAuthor);
+  });
+
+  // update the author database
+  database.authors.forEach((author) => {
+    if (author.id === req.body.newAuthor)
+      return author.books.push(req.params.isbn);
+  });
+
+  return res.json({
+    books: database.books,
+    authors: database.authors,
+    message: "New author was added 🚀",
+  });
+});
+
+/*
+Route           /publication/update/book
+Description     update/add new book to a publication
+Access          PUBLIC
+Parameters      isbn
+Method          PUT
+*/
+shapeAI.put("/publication/update/book/:isbn", (req, res) => {
+    // update the publication database
+    database.publications.forEach((publication) => {
+      if (publication.id === req.body.pubId) {
+        return publication.books.push(req.params.isbn);
+      }
+    });
+
+    // update the book database
+    database.books.forEach((book) => {
+      if (book.ISBN === req.params.isbn) {
+        book.publication = req.body.pubId;
+        return;
+      }
+   });
+
+   return res.json({
+     books: database.books,
+     publications: database.publications,
+     message: "Succesfully updated publication",
+   });
+  });
+
+/*
+Route           /book/delete
+Description     delete a book
+Access          PUBLIC
+Parameters      isbn
+Method          DELETE
+*/
+shapeAI.delete("/book/delete/:isbn", (req, res) => {
+  const updatedBookDatabase = database.books.filter(
+    (book) => book.ISBN !== req.params.isbn
+  );
+
+  database.books = updatedBookDatabase;
+  return res.json({ books: database.books });
+});
+
+/*
+Route           /book/delete/author
+Description     delete an author from a book
+Access          PUBLIC
+Parameters      isbn, authorID
+Method          DELETE
+*/
+shapeAI.delete("/book/delete/author/:isbn/:authorID", (req, res) => {
+  // update the book database
+  database.books.forEach((book) => {
+    if (book.ISBN === req.params.isbn) {
+      const newAuthorList = book.authors.filter(
+        (author) => autho !== parseInt(req.params.authorID)
+      );
+      book.authors = newAuthorList;
+      return;
+    }
+  });
+
+  // update the author database
+  database.authors.forEach((author) => {
+    if (author.id === parseInt(req.params.authorID)) {
+      const newBooksList = author.books.filter(
+        (book) => book !== req.params.isbn
+      );
+
+      author.books = newBooksList;
+      return;
+    }
+  });
+
+  return res.json({
+    message: "authors was deleted!",
+    book: database.books,
+    author: database.authors,
+  });
+});
+
+/*
+Route           /publication/delete/book
+Description     delete a book from publication
+Access          PUBLIC
+Parameters      isbn, publication ID
+Method          DELETE
+*/
+shapeAI.delete("/publication/delete/book/:isbn/:pubId", (req, res) => {
+  // update publication database
+  database.publications.forEach((publication) => {
+    if (publication.id === parseInt(req.params.pubId)) {
+      const newBooksList = publication.books.filter(
+        (book) => book !== req.params.isbn
+      );
+
+      publication.books = newBooksList;
+      return;
+    }
+  });
+
+  // update book database
+  database.books.forEach((book) => {
+    if (book.ISBN === req.params.isbn) {
+      book.publication = 0; // no publication available
+      return;
+    }
+  });
+
+  return res.json({
+    books: database.books,
+    publications: database.publications,
+  });
+});
+
+shapeAI.listen(3000, () => console.log("Server running!!😎"));
---
+++ requirements.js
@@ -0,0 +1,73 @@
+// Requirements for our project
+
+// We are a book management company
+
+// Books
+// ISBN, Title, Pub date, Language, num page, author[], category[]
+
+// Authors
+// id, name, books[]
+
+// Publications
+// id, name, books[]
+
+// What are all the APIs that we need....
+
+// Books
+// We need an API
+// To get all books *
+// To get specific books * 
+// To get list of books based on category *
+// To get list of books based on languages *
+
+// POST
+// add new book*
+
+// PUT
+// update book title*
+// update/add new author*
+
+// DELETE
+// delete a book
+// delete an author
+
+// Authors
+
+// We need an API
+
+// GET
+// to get all authors *
+// to get specific authors
+// to get list of authors based on books *
+
+// POST
+// add new author*
+
+// PUT
+// update author name*
+
+// DELETE
+// delete an author
+
+// Publications
+
+// We need an API
+
+// GET
+// to get all publications *
+// to get specific publication
+// to get list of publications based on book
+
+// POST
+// add new publications
+
+// PUT
+// update the publication name using id
+// update/add new book to a publication *
+
+// DELETE
+// delete the publication
+// delete a book from publication
+
+
+// How the server serves the request
